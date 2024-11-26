86805118f93ea8400648223f150d75d61bbdb7e1
Jayden <jieunsse@gmail.com>
2024-11-26 15:47:53 +0900

    feat : 유저 엔티티 추가

---
+++ src/main/java/gdg/waffle/BE/comment/entity/User.java
@@ -0,0 +1,33 @@
+package gdg.waffle.BE.comment.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+}
